119a934e48d82b96f6555a2e0cc27eb1baa6afe7
Daljyeong <jiyoungm1022@gmail.com>
2023-01-07 23:32:26 +0900

    design: Create 'fragment_home.xml'

---
+++ app/src/main/java/com/example/kakeibo/Data_item.kt
@@ -0,0 +1,10 @@
+package com.example.kakeibo
+
+import android.media.Image
+
+data class Data_item(
+    val icn: Image,
+    val category: String,
+    val content: String,
+    val money: String
+)
