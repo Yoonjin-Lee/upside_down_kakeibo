aee77a18e3eed7cc52ef2d3b875af2ef01f0ec27
yoonjin <peacemiller@konkuk.ac.kr>
2023-01-08 22:57:03 +0900

    week1 - login, bnb, resultShare pages

--- app/src/main/java/com/example/kakeibo/MainActivity.kt
+++ app/src/main/java/com/example/kakeibo/MainActivity.kt
@@ -2,10 +2,45 @@ package com.example.kakeibo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.kakeibo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var viewBinding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        viewBinding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(viewBinding.root)
+
+//        supportFragmentManager
+//            .beginTransaction()
+//            .replace(viewBinding.fragment.id, ) //homefragment
+//            .commitAllowingStateLoss()
+//
+//        viewBinding.bnb.run {
+//            setOnItemSelectedListener {
+//                when(it.itemId) {
+//                    R.id.home -> {
+//                        supportFragmentManager
+//                            .beginTransaction()
+//                            .replace(viewBinding.fragment.id, ) //homefragment
+//                            .commitAllowingStateLoss()
+//                    }
+//                    R.id.history -> {
+//                        supportFragmentManager
+//                            .beginTransaction()
+//                            .replace(viewBinding.fragment.id, ) //historyfragment
+//                            .commitAllowingStateLoss()
+//                    }
+//                    R.id.mypage -> {
+//                        supportFragmentManager
+//                            .beginTransaction()
+//                            .replace(viewBinding.fragment.id, ) //mypagefragment
+//                            .commitAllowingStateLoss()
+//                    }
+//                }
+//                true
+//            }
+//            selectedItemId = R.id.home
+//        }
     }
 }
