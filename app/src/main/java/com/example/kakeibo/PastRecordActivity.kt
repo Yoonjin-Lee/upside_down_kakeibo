6d5c86c831ac0b5b71e21597a74e41442a5a9154
Amepistheo <kellylhji@gmail.com>
2023-01-11 23:08:18 +0900

    add : 이전 저축 기록 & item 생성 (xml)

---
+++ app/src/main/java/com/example/kakeibo/PastRecordActivity.kt
@@ -0,0 +1,11 @@
+package com.example.kakeibo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PastRecordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_past_record)
+    }
+}
