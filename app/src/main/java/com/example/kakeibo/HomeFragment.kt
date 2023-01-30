1b061de630e10b3260736a3c40c7882b28442a29
Daljyeong <jiyoungm1022@gmail.com>
2023-01-31 02:42:54 +0900

    design: Round two corners of dialog 'fragment_add_history_main.xml'

--- app/src/main/java/com/example/kakeibo/HomeFragment.kt
+++ app/src/main/java/com/example/kakeibo/HomeFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.kakeibo.databinding.FragmentHomeBinding
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 class HomeFragment : Fragment() {
     private lateinit var viewBinding: FragmentHomeBinding
@@ -19,6 +20,10 @@ class HomeFragment : Fragment() {
 
         val bottomSheetDialogFragment = AddHistoryMainFragment()
 
+        //모서리 둥글게
+        bottomSheetDialogFragment.setStyle(BottomSheetDialogFragment.STYLE_NORMAL, R.style.RoundCornerBottomSheetDialogTheme)
+
+        //btnHomeAdd('+' 버튼) 눌렀을 때 dialog 뜨도록 구현
         viewBinding.btnHomeAdd.setOnClickListener{
             bottomSheetDialogFragment.show(parentFragmentManager, bottomSheetDialogFragment.tag)
         }
