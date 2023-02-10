2e12001344962148041cb52dc428d8e053ae3013
Daljyeong <jiyoungm1022@gmail.com>
2023-02-11 05:02:33 +0900

    feat: Make recyclerview in 'HomeFragment.kt'

--- app/src/main/java/com/example/kakeibo/Data_item.kt
+++ app/src/main/java/com/example/kakeibo/Data_item.kt
@@ -3,8 +3,9 @@ package com.example.kakeibo
 import android.media.Image
 
 data class Data_item(
-    val icn: Image,
-    val category: String,
-    val content: String,
-    val money: String
+//    val itemIcn: Image, //아이콘 이미지
+    val itemIcn: Int, //아이콘 이미지
+    val itemContent: String, //내용
+    val itemMoney: String, //금액 입력
+    val itemMemo: String //한 줄 메모
 )
